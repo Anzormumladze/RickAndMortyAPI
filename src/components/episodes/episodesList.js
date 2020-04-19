bdac98727198ebfc031109c480b999189b06ed8d
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-04-19 18:41:27 +0400

    making episodelist component and showing data

--- src/components/episodes/episodesList.js
+++ src/components/episodes/episodesList.js
@@ -1,20 +1,94 @@
-import React from 'react'
+import React from "react";
 import { connect } from "react-redux";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import Pagination from "@material-ui/lab/Pagination";
 
-const EpisodesList = ({getData}) =>{
-    console.log(getData)
-    return(
-        <div>
-            {getData.info?getData.info.count:null}
-        </div>
-    )
-}
+const useStyles = makeStyles({
+  cardContainer: {
+    display: "flex",
+    flexFlow: "row wrap",
+    justifyContent: "space-around",
+  },
+  card: {
+    marginBottom: "40px",
+    width: "350px",
+  },
+  paginationContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "10px",
+  },
+});
 
-const mapStateToProps = state => {
-    return {
-        getData: state.episodes.fetchData
-    };
+const EpisodesList = ({ getData }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <div className={classes.cardContainer}>
+        {getData.results
+          ? getData.results.map((data) => {
+              return (
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      color="textSecondary"
+                      noWrap
+                    >
+                      ID:{data.id}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography className={classes.title} color="textSecondary">
+                      EPISODE NAME: {data.name}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      color="textSecondary"
+                      noWrap
+                    >
+                      {data.air_date}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      color="textSecondary"
+                      noWrap
+                    >
+                      {data.episode}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="medium">show more</Button>
+                    <Button startIcon={<FavoriteBorderIcon />} size="medium">
+                      ADD
+                    </Button>
+                  </CardActions>
+                </Card>
+              );
+            })
+          : null}
+      </div>
+      <div className={classes.paginationContainer}>
+        <Pagination count={2} />
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    getData: state.episodes.fetchData,
   };
-  
+};
 
 export default connect(mapStateToProps)(EpisodesList);
